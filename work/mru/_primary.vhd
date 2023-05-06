0da2b6ecfd5cbab722a009466f39aad1b501a1a8
Deivid Gabriel <deividgabrielproc@gmail.com>
2023-05-06 16:25:54 -0300

    MRU and Testbench created

---
+++ work/mru/_primary.vhd
@@ -0,0 +1,18 @@
+library verilog;
+use verilog.vl_types.all;
+entity mru is
+    port(
+        clk             : in     vl_logic;
+        rst             : in     vl_logic;
+        b1              : in     vl_logic;
+        b2              : in     vl_logic;
+        b3              : in     vl_logic;
+        b4              : in     vl_logic;
+        b5              : in     vl_logic;
+        l1              : out    vl_logic;
+        l2              : out    vl_logic;
+        l3              : out    vl_logic;
+        l4              : out    vl_logic;
+        l5              : out    vl_logic
+    );
+end mru;
---
+++ work/tb_mru/_primary.vhd
@@ -0,0 +1,4 @@
+library verilog;
+use verilog.vl_types.all;
+entity tb_mru is
+end tb_mru;
